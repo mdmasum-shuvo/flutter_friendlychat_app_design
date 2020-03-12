3eb4ac794b185805916d1f79243a8316cc20c06b
mdmasum.shuvo <mdmasum.shuvo.cse@gmail.com>
2020-03-12 15:14:19 +0600

    text message list

--- lib/main.dart
+++ lib/main.dart
@@ -21,14 +21,34 @@ class ChatScreen extends StatefulWidget {
 
 class ChatScreenState extends State<ChatScreen> {
   final _textController = TextEditingController();
+  final List<ChatMessage> _message = <ChatMessage>[];
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      appBar: AppBar(title: new Text('Friendly Chat')),
-      body: _buildTextComposer(),
-    );
+        appBar: AppBar(title: new Text('Friendly Chat')),
+        body: new Column(
+          children: <Widget>[
+            Flexible(
+              child: ListView.builder(
+                padding: EdgeInsets.all(8.0),
+                reverse: true,
+                itemBuilder: (
+                  _,
+                  int index,
+                ) =>
+                    _message[index],
+                itemCount: _message.length,
+              ),
+            ),
+            Divider(height: 1.0),
+            Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextComposer(),
+            )
+          ],
+        ));
   }
 
   Widget _buildTextComposer() {
@@ -59,11 +79,20 @@ class ChatScreenState extends State<ChatScreen> {
 
   void _handleSubmitted(String text) {
     _textController.clear();
+
+    ChatMessage chatMessage = new ChatMessage(
+      text: text,
+    );
+
+    setState(() {
+      _message.insert(0, chatMessage);
+    });
   }
 }
 
 class ChatMessage extends StatelessWidget {
   ChatMessage({this.text});
+
   final String text;
 
   @override
